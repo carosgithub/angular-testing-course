533694cdb2848b6aaa5470ea7082da9971116f32
carosgithub <oscarnick@gmail.com>
2022-01-16 20:47:12 -0800

    commit

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -3,21 +3,26 @@ import { LoggerService } from "./logger.service";
 
 describe('Calculator Service', () => {
 
+  let loggerSpy: any,
+      calculator: CalculatorService;
+
+  beforeEach(() => {
+    loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
+    calculator = new CalculatorService(loggerSpy);
+  })
+
   it('It should add two numbers', () => {
-    const logger = jasmine.createSpyObj('LoggerService', ['log']);
-    const calculator = new CalculatorService(logger);
+
     const result = calculator.add(2, 3);
-
     expect(result).toBe(5);
-
-    expect(logger.log).toHaveBeenCalledTimes(1);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 
   it('It should substract two numbers', () => {
-    const calculator = new CalculatorService(new LoggerService());
-    const result = calculator.subtract(3, 2);
 
+    const result = calculator.subtract(3, 2);
     expect(result).toBe(1, 'Unexpected substraction error');
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 
 })
