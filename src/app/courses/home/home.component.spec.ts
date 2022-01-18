e131e128aa8dde1bdeb7351bf17d92bb489bd635
OscarNarvaez <oscar.l.narvaez-jimbo@dhsoha.state.or.us>
2022-01-18 10:48:41 -0800

    commit

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -24,6 +24,9 @@ describe('HomeComponent', () => {
   const beginnerCourses = setupCourses()
     .filter(course => course.category == 'BEGINNER');
 
+  const advancesCourses = setupCourses()
+  .filter(course => course.category == 'ADVANCED');
+
   beforeEach(waitForAsync(() => {
 
     const coursesServiceSpy = jasmine.createSpyObj('CoursesService', ['findAllCourses']);
@@ -67,14 +70,26 @@ describe('HomeComponent', () => {
 
   it("should display only advanced courses", () => {
 
-      pending();
+    coursesService.findAllCourses.and.returnValue(of(advancesCourses));
+
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css('.mat-tab-label'));
+
+    expect(tabs.length).toBe(1, 'Unexpected number of tabs found');
 
   });
 
 
   it("should display both tabs", () => {
 
-    pending();
+    coursesService.findAllCourses.and.returnValue(of(setupCourses()));
+
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css('.mat-tab-label'));
+
+    expect(tabs.length).toBe(2, 'Expected to find 2 tabs');
 
   });
 
