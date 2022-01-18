df9b81a1de09a9ecb5e35594d5e9611a211da48e
carosgithub <oscarnick@gmail.com>
2022-01-17 20:12:52 -0800

    commit

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -1,4 +1,4 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 import {CoursesCardListComponent} from './courses-card-list.component';
 import {CoursesModule} from '../courses.module';
 import {COURSES} from '../../../../server/db-data';
@@ -7,21 +7,29 @@ import {By} from '@angular/platform-browser';
 import {sortCoursesBySeqNo} from '../home/sort-course-by-seq';
 import {Course} from '../model/course';
 import {setupCourses} from '../common/setup-test-data';
+import { assert } from 'console';
 
 describe('CoursesCardListComponent', () => {
 
+  let component: CoursesCardListComponent,
+      fixture: ComponentFixture<CoursesCardListComponent>;
 
-  beforeEach(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
         CoursesModule
       ]
-    })
-  });
+    }).compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(CoursesCardListComponent);
+
+        component = fixture.componentInstance;
+      });
+  }));
 
   it("should create the component", () => {
 
-   fail();
+    expect(component).toBeTruthy();
 
   });
 
